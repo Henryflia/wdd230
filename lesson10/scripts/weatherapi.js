1828174f5959daef4260f3e06288ceb18521a5f6
henryflia <superflia2059@gmail.com>
2023-03-08 21:48:14 -0400

    text

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,6 +1,6 @@
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const currenttemp = document.querySelector('#current-temp');
+const weathericon = document.querySelector('#weather-icon');
+const captiondesc = document.querySelector('figcaption');
 
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=c52492675b8cb006d3a92aecf168a006"
 
@@ -9,7 +9,7 @@ async function apiFetch() {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // this is for testing the call
+        console.log(data); //
         displayResults(data);
       } else {
           throw Error(await response.text());
@@ -23,12 +23,12 @@ apiFetch();
 
 function displayResults(weatherdata) {
     console.log(weatherdata)
-    currentTemp.innerHTML = `<strong>${weatherdata.main.temp.toFixed(0)}</strong>`
+    currenttemp.innerHTML = `<strong>${weatherdata.main.temp.toFixed(0)}</strong>`
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherdata.weather[0].icon}.png`;
     const description = weatherdata.weather[0].description;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', description);
-    weatherIcon.setAttribute('loading', 'lazy')
-    captionDesc.textContent = description.toUpperCase();
+    weathericon.setAttribute('src', iconsrc);
+    weathericon.setAttribute('alt', description);
+    weathericon.setAttribute('loading', 'lazy')
+    captiondesc.textContent = description.toUpperCase();
 }
